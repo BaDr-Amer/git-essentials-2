edb2bf20485a505c44698a78caafe8e234105630
ZaidHannoun <zaidhannoun98@gmail.com>
2022-04-02 16:16:36 +0300

    mongo

--- routes/users.js
+++ routes/users.js
@@ -1,33 +1,47 @@
 import express from 'express';
 const router = express.Router()
+import User from'../models/User.js'
 
-const users = [
-    {
-        id: 1,
-        name: 'John',
-        profile: {
-            address1: 'address 1',
-            address2: 'address 2'
-        }
-    },
-    {
-        id: 2,
-        name: 'Doe',
-        profile: {
-            address1: 'address 3',
-            address2: 'address 4'
-        }
-    },
-    {
-        id: 3,
-        name: 'mark',
-        profile: {
-            address1: 'address 5',
-            address2: 'address 6'
-        }
-    }
-]
-const authentication = (req, res, next) => {
+
+router.post('/',async (req,res)=>{
+const {email,password}=req.body
+const user= await User.create({email,password})
+res.send(user)
+
+})
+
+/*router.get('/', async (req, res) => {
+const user= await User.find() 
+return res.send(user)
+})*/
+router.get('/:id', async (req,res)=>{
+//const user = await User.findOne({ _id : req.params.id})
+const user = await User.findById(req.params.id)
+return res.send(user)
+
+})
+
+router.put('/:id', (req, res) => {
+    
+})
+
+router.delete('/', async (req, res) => {
+await User.deleteOne({_id: req.params.id})
+res.status()
+const user =user.findById(req.params.id)
+if(!user){
+    throw new Error('user not found')
+}
+user.delete()
+res.status(204).send()
+})
+
+router.patch('/', (req, res) => {
+
+})
+
+export default router
+/*const authentication = (req, res, next) => {
     // validation
     if (req.isAuthenticated) {
         next()
@@ -48,24 +62,4 @@ const handler = (req, res) => {
     console.log("processing the request")
     users.push(req.body)
     res.send(req.body)
-}
-
-router.post('/', authentication, autheriztion, handler)
-
-router.get('/', (req, res) => {
-    res.send("GET request")
-})
-
-router.put('/', (req, res) => {
-
-})
-
-router.delete('/', (req, res) => {
-
-})
-
-router.patch('/', (req, res) => {
-
-})
-
-export default router
+}*/
--- server.js
+++ server.js
@@ -1,20 +1,27 @@
 import express from "express";
+import mongoose from "mongoose";
 import userRouter from "./routes/users.js"
-import profilesRouter from "./routes/profiles.js"
+
 
 const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.use('/', (req, res, next) => {
+/*app.use('/', (req, res, next) => {
     req.isAuthenticated = true
     req.isAuthorized = true
     next()
-})
+})*/
+async function connect() {
+    await mongoose.connect('mongodb://localhost:27018/base');
+  }
+  connect().then(()=> {
+    const app = express();
+    app.use(express.json())
+    app.use(express.urlencoded({ extended: true }))
+    app.use('/users', userRouter)
 
-app.use('/users', userRouter)
-app.use('/profile', profilesRouter)
-
-app.listen(3000, () => {
+    app.listen(3000, () => {
     console.log("server is listening on port 3000");
-})
+    })
+}).catch(err=> console.log(err))
