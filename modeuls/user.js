557b05bac0c7e98f27a925e975e22e101a52fcf2
AdhamSous0 <90273640+AdhamSous0@users.noreply.github.com>
2022-04-04 15:15:12 +0300

    My message

--- modeuls/user.js
+++ modeuls/user.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
-const schemauser =new mongoose.Schema({
-    email:String,
-    password:String
-})//عشان تقيد الادخال بمعنا عشان الاب ما يصير يضف على راحنه بحدد شو احط في الدتا 
- export default mongoose.model('fd',schemauser)//user name of colection in data base
+const schemauser = new mongoose.Schema({
+  email: String,
+  password: String,
+}); //عشان تقيد الادخال بمعنا عشان الاب ما يصير يضف على راحنه بحدد شو احط في الدتا
+export default mongoose.model("fd", schemauser); //user name of colection in data base
