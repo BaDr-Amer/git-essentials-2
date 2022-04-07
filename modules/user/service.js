bcc579c6a05196cbd0947c2702859067b45f0943
Ansam1999 <ansamghoul99@gmail.com>
2022-04-07 11:05:05 +0300

    4/7

--- modules/user/service.js
+++ modules/user/service.js
@@ -1,27 +1,37 @@
-import User from '../../models/User.js'
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
-import fs from 'fs'
+import User from "../../models/User.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import fs from "fs";
 
 export const create = async ({ email, password, firstName, lastName }) => {
-    const hash = await bcrypt.hash(password, 3)
-    return await User.create({ email, password: hash, firstName, lastName })
-}
+  const hash = await bcrypt.hash(password, 3);
+  return await User.create({ email, password: hash, firstName, lastName });
+};
 
 export const login = async ({ email, password }) => {
-    const user = await findByEmail(email)
-    if (!user) return Promise.reject('incorrect email or password')
+  const user = await findByEmail(email);
+  if (!user) return Promise.reject("incorrect email or password");
 
-    const passwordMatch = await bcrypt.compare(password, user.password)
-    if (!passwordMatch) return Promise.reject('incorrect email or password')
-    const token = jwt.sign({
-        _id: user._id,
-        email: user.email
-    }, fs.readFileSync('./privateKey'), { algorithm: 'RS256' });
+  const passwordMatch = await bcrypt.compare(password, user.password);
+  if (!passwordMatch) return Promise.reject("incorrect email or password");
+  const token = jwt.sign(
+    {
+      _id: user._id,
+      email: user.email,
+    },
+    fs.readFileSync("./privateKey"),
+    { algorithm: "RS256" }
+  );
 
-    return token
-}
+  return token;
+};
 
-export const findByEmail = async email => {
-    return await User.findOne({ email })
-}
+export const findByEmail = async (email) => {
+  return await User.findOne({ email });
+};
+
+export const createPost = async ({ email, content, Date, token }) => {
+  let decoded = jwt.verify(token, "shhhhh");
+  console.log(decoded.foo);
+  return await Post.createPost({ email, content, Date, decoded });
+};
