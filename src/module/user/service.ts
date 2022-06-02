1bc823000077adb57296753962599914a387f1dc
Zaid Hannoun <zaidhannoun98@gmail.com>
2022-06-02 12:00:26 +0300

    usedAgendaWithRedis

--- src/module/user/service.ts
+++ src/module/user/service.ts
@@ -2,41 +2,46 @@ import User, { IUser } from '../../model/User'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import fs from 'fs'
-import Reminder from  '../../model/reminder'
+import Reminder from '../../model/reminder'
 import { ApiError } from '../../errors/ApiError'
-import  Redis  from 'ioredis'
+import Redis from 'ioredis'
 import pushNotifications from "../../queues/pushnotifications"
 
 import eventEmitter from "events"
 
-const redis =new Redis({});
+const redis = new Redis({
+    port: 6379,
+    host: "localhost"
+});
 
-export const create = async ({ email , password, firstName, lastName ,middleName,isInfected ,createdAt,updatedAt })  => {
+export const create = async ({ email, password, firstName, lastName, middleName, isInfected, createdAt, updatedAt }) => {
     const hash = await bcrypt.hash(password, 3)
-    let reminder 
-   
-    const user  = await User.create({ email, password: hash, firstName, lastName  ,createdAt,updatedAt/*,reminder*/})
-    if(!firstName||!lastName){
-        reminder = await Reminder.findOne({ reminderName : "Registration"})
-        pushNotifications.add({...reminder._doc , user_id : user.id }) 
+    let reminder
+
+    const user = await User.create({ email, password: hash, firstName, lastName, createdAt, updatedAt/*,reminder*/ })
+    if (!firstName || !lastName) {
+        reminder = await Reminder.findOne({ reminderName: "Registration" })
+        pushNotifications.add({ ...reminder._doc, user_id: user.id, date: new Date().getTime() })
+     const d=   await redis.set('notificationReminder'+user.id,  user.id)
+     console.log(d)
     }
 
     return user
 }
 
-export const login = async ({ email, password  }) => {
+export const login = async ({ email, password }) => {
     const user = await findByEmail(email)
-    if (!user) throw  ApiError.wrongEmailOrPassword('wrongEmailOrPassword')
+    if (!user) throw ApiError.wrongEmailOrPassword('wrongEmailOrPassword')
 
     const passwordMatch = await bcrypt.compare(password, user.password)
-    if (!passwordMatch) throw  ApiError.wrongEmailOrPassword('wrongEmailOrPassword')
+    if (!passwordMatch) throw ApiError.wrongEmailOrPassword('wrongEmailOrPassword')
     const token = jwt.sign({
         _id: user._id,
         email: user.email,
-        isInfected :user.isInfected
+        isInfected: user.isInfected
     }, fs.readFileSync('./privateKey'), { algorithm: 'RS256' });
-    
-    return {token ,user}
+
+    return { token, user }
 
 }
 
@@ -44,10 +49,10 @@ export const findByEmail = async email => {
     return await User.findOne({ email })
 }
 
-export const changeInfection=async  (userId)=> {
-const user :IUser =await User.findOneAndUpdate(userId, [{$set:{isInfected:{$eq:[false,"$isInfected"]}}}])
-if (!user)
-throw ApiError.userNotFoundError('userNotFound')
+export const changeInfection = async (userId) => {
+    const user: IUser = await User.findOneAndUpdate(userId, [{ $set: { isInfected: { $eq: [false, "$isInfected"] } } }])
+    if (!user)
+        throw ApiError.userNotFoundError('userNotFound')
 
 }
 
@@ -56,12 +61,12 @@ throw ApiError.userNotFoundError('userNotFound')
 
 
 
-export const updateMany = async( { firstName, updatedFirstName , updatedAt :updatedAt})=> {
-return await User.updateMany({firstName},{$set : {firstName : updatedFirstName , updatedAt }})
+export const updateMany = async ({ firstName, updatedFirstName, updatedAt: updatedAt }) => {
+    return await User.updateMany({ firstName }, { $set: { firstName: updatedFirstName, updatedAt } })
 }
 
-export const deleteByFirstName = async({firstName})=> {
-return await User.delete({firstName})
+export const deleteByFirstName = async ({ firstName }) => {
+    return await User.delete({ firstName })
 
 
 }
--- src/queues/pushNotifications.ts
+++ src/queues/pushNotifications.ts
@@ -4,27 +4,48 @@ import Notification from '../model/Notification'
 import Agenda from "agenda";
 import humanInterval from 'human-interval';
 import User from '../model/User'
+import agenda from '../jobs/agenda';
+import Redis from 'ioredis';
 
 const queue = new bull('pushNotifications', connection)
-
+const redis = new Redis({
+    port: 6379,
+    host: "localhost"
+});
 
 
 queue.process(async (jobs) => {
     const agenda = new Agenda({ db: { address: "mongodb://127.0.0.1:27018", collection: "agendaJobs" } });
-    agenda.define('notificationJob', async job => {
-         const user= await User.findById(jobs.data.user_id)
-        if(jobs.data.repetition>0&&(!user.firstName||!user.lastName)){
-        const { user_id, text  } = job.attrs.data
-        await Notification.create({ user_id, text })
-        jobs.data.repetition--;
-        job.repeatEvery(jobs.data.interval)}
-        
+    agenda.define('notificationJob' + jobs.data.user_id, async job => {
+        const user = await redis.get('notificationReminder' + jobs.data.user_id)
+        const { user_id, text, count } = job.attrs.data
+        if ((user != null)) {
+            if (count <= 0) {
+                await job.remove();
+                await redis.del('notificationReminder' + jobs.data.user_id)
+            }
+            else {
+                job.attrs.data.count--
+                await Notification.create({ user_id, text })
+                job.repeatEvery(jobs.data.interval)
+            }
+        }
+
     })
     await agenda.start()
     if (jobs.data.type == 'after') {
-        await agenda.schedule(jobs.data.scheduleAfter, 'notificationJob', { user_id: jobs.data.user_id, text: "complete ur profile" })
+        // endDate = new Date(new Date(jobs.data.date + (humanInterval(jobs.data.interval) * jobs.data.repetition)))
+        await agenda.schedule(
+          jobs.data.scheduleAfter,
+          "notificationJob" + jobs.data.user_id,
+          {
+            user_id: jobs.data.user_id,
+            text: "complete ur profile",
+            count: jobs.data.repetition,
+          }
+        );
     }
-    else {jobs.data.type =='before' }
+    else { jobs.data.type == 'before' }
 
 })
 
